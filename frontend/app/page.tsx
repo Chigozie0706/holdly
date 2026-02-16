a36be5c308cd44cb90175c01d7a77dc801144084
chigozie0706 <chigoziejacob@gmail.com>
2026-02-16 19:38:38 +0100

    updated imports

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -9,7 +9,6 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { useStacks } from "@/providers/stacks-provider";
 import { request } from "@stacks/connect";
-import { Cl, PostConditionMode } from "@stacks/transactions";
 import { fetchCallReadOnlyFunction } from "@stacks/transactions";
 import { STACKS_TESTNET } from "@stacks/network";
 import { uintCV, stringUtf8CV, cvToJSON } from "@stacks/transactions";
