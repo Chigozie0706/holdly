9db0d3382ad780500f464bbdee0fc0a046a8f4cc
chigozie0706 <chigoziejacob@gmail.com>
2026-03-19 18:04:37 +0100

    added some constannt and page dec

--- frontend/app/my_listed_books/page.tsx
+++ frontend/app/my_listed_books/page.tsx
@@ -6,3 +6,9 @@ import Footer from "@/components/Footer";
 import MyListedBooks from "@/components/MyListedBooks";
 import { useStacks } from "@/providers/stacks-provider";
 import { toast } from "sonner";
+
+const CONTRACT_ADDRESS = "SP3N8PR8ARF68BC45EDK4MWZ3WWDM74CFJB3SS99R";
+const CONTRACT_NAME = "holdlyv8";
+const DEPOSIT_AMOUNT = 1000000;
+
+export default function MyListedBooksPage() {
