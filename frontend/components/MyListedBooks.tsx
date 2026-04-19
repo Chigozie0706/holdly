c427c2ac0eccdf099864db3691fe99140af68ce3
chigozie0706 <chigoziejacob@gmail.com>
2026-04-19 23:38:46 +0100

    adding component EditRow

--- frontend/components/MyListedBooks.tsx
+++ frontend/components/MyListedBooks.tsx
@@ -462,6 +462,7 @@ export default function MyListedBooks({
 
               /* Inline edit row */
                               {editingId === book.id && (
+                  <EditRow
 
             ))}
           </tbody>
