e621d4e367ae8e50a6c219a115a9407f2c8e710f
chigozie0706 <chigoziejacob@gmail.com>
2026-05-09 09:35:50 +0100

    creating variable connected

--- frontend/providers/stacks-provider1.tsx
+++ frontend/providers/stacks-provider1.tsx
@@ -24,4 +24,5 @@ export function StacksProvider({ children }: { children: ReactNode }) {
   const [address, setAddress] = useState<string | null>(null);
   const [btcAddress, setBtcAddress] = useState<string | null>(null);
   const [publicKey, setPublicKey] = useState<string | null>(null);
+  const [connected, setConnected];
 }
