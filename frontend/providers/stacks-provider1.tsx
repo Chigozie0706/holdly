04ba78925b71ab4b2641571087fa5ffd2518ed0d
chigozie0706 <chigoziejacob@gmail.com>
2026-05-15 22:43:59 +0100

    added if statement

--- frontend/providers/stacks-provider1.tsx
+++ frontend/providers/stacks-provider1.tsx
@@ -82,6 +82,10 @@ export function StacksProvider({ children }: { children: ReactNode }) {
 
         const stxAccount = addresses.find((acc: any) => acc.symbol === "STX")
         const btcAccount = addresses.find((acc: any) => acc.symbol === "BTC")
+
+        if () {
+          
+        }
       }
     }
   });
