3ac1cf443a6ddc35b80eb58472d7583bb36750ea
chigozie0706 <chigoziejacob@gmail.com>
2026-05-18 13:19:35 +0100

    set alignItems  value to 0.3rem

--- frontend/components/BookCard.tsx
+++ frontend/components/BookCard.tsx
@@ -74,7 +74,11 @@ export default function BookCard({
   function StarRating({ average, count }: { average: number; count: number }) {
     const stars = average / 10;
 
-    return <div style={{ display: "flex", alignItems: "center", gap: }}></div>;
+    return (
+      <div
+        style={{ display: "flex", alignItems: "center", gap: "0.3rem" }}
+      ></div>
+    );
   }
 
   const handleEditFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
