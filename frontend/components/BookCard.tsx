0316ff1feffdce0fa2af17729b25072085b12a87
chigozie0706 <chigoziejacob@gmail.com>
2026-05-18 13:18:30 +0100

    added gap prop to style

--- frontend/components/BookCard.tsx
+++ frontend/components/BookCard.tsx
@@ -74,7 +74,7 @@ export default function BookCard({
   function StarRating({ average, count }: { average: number; count: number }) {
     const stars = average / 10;
 
-    return <div style={{ display: "flex", alignItems: "center" }}></div>;
+    return <div style={{ display: "flex", alignItems: "center", gap: }}></div>;
   }
 
   const handleEditFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
