8065a29131efbe83cb57b53bc09de2244b0f1964
chigozie0706 <chigoziejacob@gmail.com>
2026-05-19 21:18:56 +0100

    added size value: 0.7rem

--- frontend/components/BookCard.tsx
+++ frontend/components/BookCard.tsx
@@ -77,7 +77,7 @@ export default function BookCard({
     return (
       <div style={{ display: "flex", alignItems: "center", gap: "0.3rem" }}>
         {[1, 2, 3, 4, 5].map((star) => (
-          <span key={star} style={{ fontSize: }}></span>
+          <span key={star} style={{ fontSize: "0.7rem" }}></span>
         ))}
       </div>
     );
