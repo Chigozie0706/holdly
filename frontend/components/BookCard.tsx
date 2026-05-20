5da6b406f80525c87a54ec2744d320a216a006cd
chigozie0706 <chigoziejacob@gmail.com>
2026-05-20 15:17:36 +0100

    added alignItems  prop to style

--- frontend/components/BookCard.tsx
+++ frontend/components/BookCard.tsx
@@ -390,7 +390,7 @@ export default function BookCard({
                     {book["deposit-token"] || "STX"}
                   </span>
                 </div>
-                <div style={{ display: "flex", flexDirection: "column" }}>
+                <div style={{ display: "flex", flexDirection: "column", alignItems : }}>
                   <div className="book-meta-borrows">
                     <Users size={11} />
                     {book["total-borrows"]}x
