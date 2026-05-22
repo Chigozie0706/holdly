ff6c00407fd550f94257dcba8d2e7200a427febc
chigozie0706 <chigoziejacob@gmail.com>
2026-05-22 18:57:33 +0100

    added type number

--- frontend/components/MyBorrows.tsx
+++ frontend/components/MyBorrows.tsx
@@ -36,7 +36,7 @@ export default function MyBorrows({
     0,
   );
 
-  const handleRate = async (bookId: number, score:) => {};
+  const handleRate = async (bookId: number, score: number) => {};
 
   return (
     <>
