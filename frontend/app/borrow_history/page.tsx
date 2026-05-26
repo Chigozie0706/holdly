0fac71afd1a1c30bc0a8fcaac58746986dd3db46
chigozie0706 <chigoziejacob@gmail.com>
2026-05-26 09:59:11 +0100

    changed useStacks import location

--- frontend/app/borrow_history/page.tsx
+++ frontend/app/borrow_history/page.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import Header from "@/components/HeaderWrapper";
 import Footer from "@/components/Footer";
-import { useStacks } from "@/providers/stacks-provider";
+import { useStacks } from "@/providers/stacks-provider1";
 import { CONTRACT_ADDRESS, CONTRACT_NAME } from "@/config/contract";
 import { BookOpen, Star } from "lucide-react";
 import { toast } from "sonner";
