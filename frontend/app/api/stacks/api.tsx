44179c0cbc89ba882e6df873e66268d5bcea0f79
chigozie0706 <chigoziejacob@gmail.com>
2026-06-01 03:36:59 +0100

    reading contractAddress and name from frontend

--- frontend/app/api/stacks/api.tsx
+++ frontend/app/api/stacks/api.tsx
@@ -1,3 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest) {}
+export async function POST(req: NextRequest) {
+      try {
+            const { contractAddress, contractName, functionName, functionArgs } =
+      await req.json();
+
+      }
+}
