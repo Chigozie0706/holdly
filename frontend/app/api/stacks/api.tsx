ed5c9179938b01e8da1530ba834264a467a0783d
chigozie0706 <chigoziejacob@gmail.com>
2026-06-01 03:40:44 +0100

    added catch method to it

--- frontend/app/api/stacks/api.tsx
+++ frontend/app/api/stacks/api.tsx
@@ -1,10 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-      try {
-            const { contractAddress, contractName, functionName, functionArgs } =
+  try {
+    const { contractAddress, contractName, functionName, functionArgs } =
       await req.json();
     const url = `https://api.mainnet.hiro.so/v2/contracts/call-read/${contractAddress}/${contractName}/${functionName}`;
-
-      }
+  } catch {}
 }
