ac757a7fd21f10f582a61348c6a99abccc237bf1
chigozie0706 <chigoziejacob@gmail.com>
2026-06-01 03:39:55 +0100

    created const url read contract endpoint from Hiro

--- frontend/app/api/stacks/api.tsx
+++ frontend/app/api/stacks/api.tsx
@@ -4,6 +4,7 @@ export async function POST(req: NextRequest) {
       try {
             const { contractAddress, contractName, functionName, functionArgs } =
       await req.json();
+    const url = `https://api.mainnet.hiro.so/v2/contracts/call-read/${contractAddress}/${contractName}/${functionName}`;
 
       }
 }
