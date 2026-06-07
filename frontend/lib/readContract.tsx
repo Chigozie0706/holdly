489e4282ec78beb8165b900b53b5e5ae44e52602
chigozie0706 <chigoziejacob@gmail.com>
2026-06-07 13:07:25 +0100

    added params: ReadContractParams

--- frontend/lib/readContract.tsx
+++ frontend/lib/readContract.tsx
@@ -5,17 +5,7 @@ import {
   ClarityValue,
 } from "@stacks/transactions";
 
-export async function readContract({
-  contractAddress,
-  contractName,
-  functionName,
-  functionArgs = [],
-}: {
-  contractAddress: string;
-  contractName: string;
-  functionName: string;
-  functionArgs?: ClarityValue[];
-}) {
+export async function readContract(params: ReadContractParams) {
   // Serialize args to hex
   const serializedArgs = functionArgs.map((arg) =>
     Buffer.from(serializeCV(arg)).toString("hex"),
