88098a21fec3b88c8a7629b4f497c7d82b5d713f
chigozie0706 <chigoziejacob@gmail.com>
2026-06-23 20:58:45 +0100

    return new Date

--- frontend/lib/blockTime.tsx
+++ frontend/lib/blockTime.tsx
@@ -4,4 +4,6 @@ const MINUTES_PER_BLOCK = 10;
 export function blocksToDate(currentBlock: number, targetBlock: number): Date {
   const blocksRemaining = targetBlock - currentBlock;
   const minutesRemaining = blocksRemaining * MINUTES_PER_BLOCK;
+
+  return new Date(Date.now());
 }
