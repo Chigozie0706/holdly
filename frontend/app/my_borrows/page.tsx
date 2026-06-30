3e4a8499c50aabb92ab0ed3a39b420ddc46982fa
chigozie0706 <chigoziejacob@gmail.com>
2026-06-30 03:28:54 +0100

    added currentBlock as props to MyBorrow component

--- frontend/app/my_borrows/page.tsx
+++ frontend/app/my_borrows/page.tsx
@@ -238,6 +238,7 @@ export default function MyBorrowsPage() {
         onReturn={handleReturn}
         connected={connected}
         isLoading={isLoading}
+        currentBlock={currentBlock}
       />
     </>
   );
