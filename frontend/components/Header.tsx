06cc035e817507530ee4e5f771a5d076675ad70a
chigozie0706 <chigoziejacob@gmail.com>
2026-07-08 02:37:38 +0100

    added new object

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -37,6 +37,7 @@ const PUBLIC_LINKS = [
 
 const PRIVATE_LINKS = [
   { href: "/my_borrows", label: "My Borrows", icon: BookOpen },
+  { href: "/my_listed_books", label: "My Listed", icon: BookCopy },
 ];
 
 export default function Header({
