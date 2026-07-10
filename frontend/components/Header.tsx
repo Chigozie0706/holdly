e1712c4dee0cc36d7ce442f3e9b1a2f32c86b994
chigozie0706 <chigoziejacob@gmail.com>
2026-07-10 05:24:18 +0100

    added label to it

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -165,6 +165,7 @@ export default function Header({
                 className={`drawer-link ${pathname === href ? "active" : ""}`}
               >
                 <Icon size={15} />
+                {label}
               </Link>
             ))}
           </>
